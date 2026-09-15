ba98bd96756509316fcc805af3d604420acb96f2
m2panda <garciaisaac468@gmail.com>
2026-09-14 20:37:53 -0600

    feat: :sparkleS: add db schema to project
    - add types for postgre db schema
    - create db schema file

---
+++ database/schema/types.sql
@@ -0,0 +1,32 @@
+CREATE TYPE USER_PROFILE AS ENUM (
+   'superadmin',
+   'administrator',
+   'borrower',
+   'investor'
+);
+
+CREATE TYPE SETTINGS_UNIT AS ENUM (
+   'percentage',
+   'monetary'
+);
+
+CREATE TYPE LOAN_TYPE AS ENUM (
+   'weekly',
+   'monthly'
+);
+
+CREATE TYPE LOAN_PAYMENT_TYPE AS ENUM (
+   'capital',
+   'regular',
+   'late'
+);
+
+CREATE TYPE PAYMENT_EXTRA_CONCEPT AS ENUM (
+   'mora',
+   'visit'
+);
+
+CREATE TYPE MOVEMENT_TYPE AS ENUM (
+   'deposit',
+   'withdrawal'
+);
