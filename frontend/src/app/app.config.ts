78de02a6e7d7b21d1a7b9d4c80fcefc7c264dd50
m2panda <garciaisaac468@gmail.com>
2026-09-20 20:11:35 -0600

    build: :hammer_and_wrench: add primeng to frontend project
    - add primeng to project and set global providers

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
 
 export const appConfig: ApplicationConfig = {
-   providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+   providers: [
+      provideBrowserGlobalErrorListeners(),
+      provideRouter(routes),
+      providePrimeNG({
+         theme: {
+            preset: Aura,
+            options: {
+               darkModeSelector: false || 'none',
+            },
+         },
+         license: '',
+      }),
+      MessageService,
+      DialogService,
+      ConfirmationService,
+   ],
 };
